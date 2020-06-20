2648e45f1c9b46cab8efa9ea1dc96b215226f4d4
System Administrator <root@iMac-de-Andres.local>
2020-06-19 22:40:47 -0600

    Updates

---
+++ precache-manifest.f29bbc3c62fabe72d85cb48bb4a7db41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa18d1ced13d32e04464ae8040e9f3d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d0189a7e0b49fe6dc817",
+    "url": "/monsters-rolodex/static/css/main.7441591b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0189a7e0b49fe6dc817",
+    "url": "/monsters-rolodex/static/js/main.f189a8d9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
